0737e9bc350d8f4290eba73085b3977f94a00c5d
BryanAbuchery <bryanabuchery5@gmail.com>
2021-07-27 15:33:11 -0300

    Updated Scripts

--- Scripts/Exploring_Phylogenetics_DECIPHER_Aln.R
+++ Scripts/Exploring_Phylogenetics_DECIPHER_Aln.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 #ensure that Tex/LaTex is also installed
-install.packages("pacman")
+#install.packages("pacman")
 library("pacman") # This code chunk shows the loading of required 
 # packages. Here, p_load() from pacman, which installs 
 # the package if necessary and loads it for use. 
@@ -26,7 +26,7 @@ index <- match(AA, uAA)
 U_AA <- AlignSeqs(uAA)
 AA_aligned <- U_AA[index]
 writeXStringSet(AA_aligned, file="DECIPHER_Aligned.fasta") #output the alignment file.
-install.packages("seqinr") #to manipulate the sequence file
+#install.packages("seqinr") #to manipulate the sequence file
 library(seqinr)
 Sqr_AA <- read.alignment("DECIPHER_Aligned.fasta", format = "fasta")
 distmatrix <- dist.alignment(Sqr_AA, matrix = c("similarity", "identity")) #calculate distance matrix
@@ -37,7 +37,7 @@ treedf <- as_tibble(phylotree)
 tips <- phylotree$tip.label
 merges <- merge(treedf, sample_data, by.x = "label", by.y = "Entry", all.x = TRUE, all.y = FALSE)
 newtips <- merges$Organism
-install.packages("phylotools")
+#install.packages("phylotools")
 library(phylotools) #Include the tree labels
 newtree <- as.phylo(merges)
 rename.tips.phylo <- function(tree, names) {
@@ -45,14 +45,36 @@ rename.tips.phylo <- function(tree, names) {
   return(tree)
 }
 tree2plot <- rename.tips.phylo(newtree, newtips)
-plots <- ggtree(tree2plot, layout="circular", branch.length = "none", size = .5)
-plots2 <- ggtree(tree2plot, branch.length = "none")
-ggsave("IP6K_tree_circular2.png", plot = plots)
-ggsave("IPK6_tree_linear2.png", plot = plots2)
-ggsave(plot = plots, filename = "Inositol_hexakisphosphate(circular)2.pdf",
-       width = 10, height = 10, dpi = "retina")
-ggsave(plot = plots2, filename = "Inositol_hexakisphosphate(linear)2.pdf",
-       width = 10, height = 10, dpi = "retina")
-
-
-
+p <- ggtree(tree2plot, layout = "circular", branch.length = 'none') %<+% sample_data + # %<+% adds dataframe with sample data to tree
+  geom_tippoint(
+    mapping = aes(color = Organism),          # tip color by Organism. You may change shape adding "shape = "
+    size = 1.5)+                               # define the size of the point at the tip
+  scale_color_brewer(
+    name = "Organism",                    # name of your color scheme (will show up in the legend like this)
+    palette = "Set1",                      # we choose a set of colors coming with the brewer package
+    na.value = "grey") +                    # for the NA values we choose the color grey
+  geom_tiplab(                             # adds name of sample to tip of its branch 
+    color = 'black',                       # (add as many text lines as you wish with + , but you may need to adjust offset value to place them next to each other)
+    offset = 1,
+    size = 1,
+    geom = "text",
+    align = TRUE)+
+  ggtitle("Phylogenetic tree of Phosphoinositol Pyrophosphates")+       # title of your graph
+  theme(
+    axis.title.x = element_blank(), # removes x-axis title
+    axis.title.y = element_blank(), # removes y-axis title
+    legend.title = element_text(    # defines font size and format of the legend title
+      face = "bold",
+      size = 12),   
+    legend.text=element_text(       # defines font size and format of the legend text
+      face = "bold",
+      size = 10),  
+    plot.title = element_text(      # defines font size and format of the plot title
+      size = 12,
+      face = "bold"),  
+    legend.position = "bottom",     # defines placement of the legend
+    legend.box = "vertical",        # defines placement of the legend
+    legend.margin = margin()) 
+ggsave("IP6K_tree_circular2.png", plot = p, width = 12, height = 14)
+ggsave(plot = p, filename = "Inositol_hexakisphosphate2.pdf",
+       width = 12, height = 14, dpi = "retina")
--- Scripts/Exploring_Phylogenetics_MSA_Aln.R
+++ Scripts/Exploring_Phylogenetics_MSA_Aln.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 #ensure that Tex/LaTex is also installed
-install.packages("pacman")
+#install.packages("pacman", repos = "http://cran.us.r-project.org")
 library("pacman") # This code chunk shows the loading of required 
                   # packages. Here, p_load() from pacman, which installs 
                   # the package if necessary and loads it for use. 
@@ -30,7 +30,7 @@ tex_file <- paste(outdir,".tex",sep="")
 msaPrettyPrint(myFirstAlignment, output="tex", showConsensus = "none", askForOverwrite=FALSE, verbose=FALSE,
                file = tex_file, alFile = fasta_file ) #writes the alignment files
 treeAln <- msaConvert(myFirstAlignment, type="seqinr::alignment") #convert alignment into a file recognized by seqinr
-install.packages("seqinr")
+#install.packages("seqinr")
 library(seqinr)
 distmatrix <- dist.alignment(treeAln, "identity")
 phylotree <- nj(distmatrix)
@@ -40,7 +40,7 @@ sample_data <- import("Inositol_hexakisphosphate.csv")
 treedtf <- as_tibble(phylotree)
 mergeddf <- merge(treedtf, sample_data, by.x = "label", by.y = "Entry", all.x =TRUE, all.y = FALSE)
 newtips <- mergeddf$Organism
-install.packages("phylotools") #plot and label tree
+#install.packages("phylotools") #plot and label tree
 library(phylotools)
 new_tree <- as.phylo(mergeddf)
 rename.tips.phylo <- function(tree, names) {
@@ -48,11 +48,36 @@ rename.tips.phylo <- function(tree, names) {
   return(tree)
 }
 tree2plot <- rename.tips.phylo(new_tree, newtips)
-plots <- ggtree(tree2plot, layout="circular", branch.length = "none", size = .5)
-plots2 <- ggtree(tree2plot, branch.length = "none")
-ggsave("IP6K_tree_circular.png", plot = plots)
-ggsave("IPK6_tree_linear.png", plot = plots2)
-ggsave(plot = plots, filename = "Inositol_hexakisphosphate(circular).pdf",
-       width = 10, height = 10, dpi = "retina")
-ggsave(plot = plots2, filename = "Inositol_hexakisphosphate(linear).pdf",
-       width = 10, height = 10, dpi = "retina")
+p <- ggtree(tree2plot, layout = "circular", branch.length = 'none') %<+% sample_data + # %<+% adds dataframe with sample data to tree
+  geom_tippoint(
+    mapping = aes(color = Organism),          # tip color by Organism. You may change shape adding "shape = "
+    size = 1.5)+                               # define the size of the point at the tip
+  scale_color_brewer(
+    name = "Organism",                    # name of your color scheme (will show up in the legend like this)
+    palette = "Set1",                      # we choose a set of colors coming with the brewer package
+    na.value = "grey") +                    # for the NA values we choose the color grey
+  geom_tiplab(                             # adds name of sample to tip of its branch 
+    color = 'black',                       # (add as many text lines as you wish with + , but you may need to adjust offset value to place them next to each other)
+    offset = 1,
+    size = 1,
+    geom = "text",
+    align = TRUE)+    
+  ggtitle("Phylogenetic tree of Phosphoinositol Pyrophosphates")+       # title of your graph
+  theme(
+    axis.title.x = element_blank(), # removes x-axis title
+    axis.title.y = element_blank(), # removes y-axis title
+    legend.title = element_text(    # defines font size and format of the legend title
+      face = "bold",
+      size = 12),   
+    legend.text=element_text(       # defines font size and format of the legend text
+      face = "bold",
+      size = 10),  
+    plot.title = element_text(      # defines font size and format of the plot title
+      size = 12,
+      face = "bold"),  
+    legend.position = "bottom",     # defines placement of the legend
+    legend.box = "vertical",        # defines placement of the legend
+    legend.margin = margin()) 
+ggsave("IP6K_tree_circular.png", plot = p, width = 12, height = 14)
+ggsave(plot = p, filename = "Inositol_hexakisphosphate.pdf",
+       width = 12, height = 14, dpi = "retina")
